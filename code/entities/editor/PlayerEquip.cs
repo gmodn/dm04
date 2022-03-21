09611079c5f5bf4244f9fc5757c226832730074e
Ian <gmanchewing@gmail.com>
2022-03-21 19:28:24 -0400

    Added all weapons to player equip and fixed FGDs

--- code/entities/editor/PlayerEquip.cs
+++ code/entities/editor/PlayerEquip.cs
@@ -5,14 +5,15 @@ using Hammer;
 using Sandbox;
 
 [Library( "dm04_playerequip" )]
-[Hammer.EntityTool( "Player Equip", "DM:04", "Gives The Player Weapons On Spawn" )]
+[Hammer.EntityTool( "Player Equip", "DM:04", "Manipulates the Player Arsenal" )]
 [Hammer.EditorSprite( "materials/dev/editor/dm04playerequip.vmat" )]
 
 public partial class PlayerSetup : Entity
 {
 
-	[Property( "EnableFlashlight" ), Description( "Is flashlight enabled or disabled on spawn" )]
-	public bool Enable_Flashlight { get; set; } = true;
+	//[Property( "EnableFlashlight" ), Description( "Is flashlight enabled or disabled on spawn" )]
+	//public bool Enable_Flashlight { get; set; } = true;
+	//Will do something. Someday.
 
 	[Property( "GiveCrowbar" ), Description( "Give the Crowbar on player spawn" )]
 	public bool Should_Give_Crowbar { get; set; } = false;
@@ -26,7 +27,25 @@ public partial class PlayerSetup : Entity
 	[Property( "GiveSMG" ), Description( "Give the SMG on player spawn" )]
 	public bool Should_Give_SMG { get; set; } = false;
 
-	[Property( "GivePistolAmmo" ), Description( "How much Pistol9 ammo to give on player spawn (Limit is 150)" )]
+	[Property( "GiveShotgun" ), Description( "Give the Shotgun on player spawn" )]
+	public bool Should_Give_Shotgun { get; set; } = false;
+
+	[Property( "GiveAR2" ), Description( "Give the AR2 on player spawn" )]
+	public bool Should_Give_AR2 { get; set; } = false;
+
+	[Property( "GiveCrossbow" ), Description( "Give the Crossbow on player spawn" )]
+	public bool Should_Give_Bow { get; set; } = false;
+
+	[Property( "GiveGrenade" ), Description( "Give the Grenades on player spawn" )]
+	public bool Should_Give_Grenade { get; set; } = false;
+
+	[Property( "GiveRPG" ), Description( "Give the RPG on player spawn" )]
+	public bool Should_Give_RPG { get; set; } = false;
+
+	[Property( "GiveSlam" ), Description( "Give the SLAM on player spawn" )]
+	public bool Should_Give_Slam { get; set; } = false;
+
+	[Property( "GivePistolAmmo" ), Description( "How much Pistol ammo to give on player spawn (Limit is 150)" )]
 	[MinMax( 0, 150 )]
 	public int Pistol_Ammo_To_Give { get; set; } = 0;
 
@@ -37,6 +56,38 @@ public partial class PlayerSetup : Entity
 	[Property( "GiveSMGAmmo" ), Description( "How much SMG ammo to give on player spawn (Limit is 225)" )]
 	[MinMax( 0, 225 )]
 	public int SMG_Ammo_To_Give { get; set; } = 0;
+
+	[Property( "GiveSMGAltAmmo" ), Description( "How much SMG alt ammo to give on player spawn (Limit is 3)" )]
+	[MinMax( 0, 3 )]
+	public int SMG_AltAmmo_To_Give { get; set; } = 0;
+
+	[Property( "GiveShotgunAmmo" ), Description( "How much Shotgun ammo to give on player spawn (Limit is 30)" )]
+	[MinMax( 0, 30 )]
+	public int Shotgun_Ammo_To_Give { get; set; } = 0;
+
+	[Property( "GiveAR2Ammo" ), Description( "How much AR2 ammo to give on player spawn (Limit is 60)" )]
+	[MinMax( 0, 60 )]
+	public int AR2_Ammo_To_Give { get; set; } = 0;
+
+	[Property( "GiveAR2AltAmmo" ), Description( "How much AR2 alt ammo to give on player spawn (Limit is 3)" )]
+	[MinMax( 0, 3 )]
+	public int AR2_AltAmmo_To_Give { get; set; } = 0;
+
+	[Property( "GiveCrossbowBolts" ), Description( "How much Crossbow bolts to give on player spawn (Limit is 4)" )]
+	[MinMax( 0, 4 )]
+	public int Crossbow_Ammo_To_Give { get; set; } = 0;
+
+	[Property( "GiveGrenades" ), Description( "How many Grenades to give on player spawn (Limit is 5)" )]
+	[MinMax( 0, 5 )]
+	public int Grenades_To_Give { get; set; } = 0;
+
+	[Property( "GiveRPGRockets" ), Description( "How many RPG rockets to give on player spawn (Limit is 3)" )]
+	[MinMax( 0, 3 )]
+	public int RPG_Ammo_To_Give { get; set; } = 0;
+
+	[Property( "GiveSlams" ), Description( "How many SLAMs to give on player spawn (Limit is 225)" )]
+	[MinMax( 0, 5 )]
+	public int Slams_To_Give { get; set; } = 0;
 
 	private List<BaseDmWeapon> weaponsToGive;
 
@@ -56,6 +107,24 @@ public partial class PlayerSetup : Entity
 
 		if ( Should_Give_SMG )
 			weaponsToGive.Add( Library.Create<BaseDmWeapon>( "hl2_smg1" ) );
+
+		if ( Should_Give_Shotgun )
+			weaponsToGive.Add( Library.Create<BaseDmWeapon>( "hl2_spas12" ) );
+
+		if ( Should_Give_AR2 )
+			weaponsToGive.Add( Library.Create<BaseDmWeapon>( "hl2_ar2" ) );
+
+		if ( Should_Give_Bow )
+			weaponsToGive.Add( Library.Create<BaseDmWeapon>( "hl2_crossbow" ) );
+
+		if ( Should_Give_Grenade )
+			weaponsToGive.Add( Library.Create<BaseDmWeapon>( "hl2_grenade" ) );
+
+		if ( Should_Give_RPG )
+			weaponsToGive.Add( Library.Create<BaseDmWeapon>( "hl2_rpg" ) );
+
+		if ( Should_Give_Slam )
+			weaponsToGive.Add( Library.Create<BaseDmWeapon>( "hl2_slam" ) );
 	}
 
 	[Event.Tick.Server]
@@ -76,6 +145,15 @@ public partial class PlayerSetup : Entity
 				player.SetAmmo( AmmoType.Pistol, Pistol_Ammo_To_Give );
 				player.SetAmmo( AmmoType.Magnum, Magnum_Ammo_To_Give );
 				player.SetAmmo( AmmoType.SMG, SMG_Ammo_To_Give );
+				player.SetAmmo( AmmoType.SMG_grenade, SMG_AltAmmo_To_Give );
+				player.SetAmmo( AmmoType.Buckshot, Shotgun_Ammo_To_Give );
+				player.SetAmmo( AmmoType.AR2, AR2_Ammo_To_Give );
+				player.SetAmmo( AmmoType.AR2_ball, AR2_AltAmmo_To_Give );
+				player.SetAmmo( AmmoType.Crossbow, Crossbow_Ammo_To_Give );
+				player.SetAmmo( AmmoType.RPG, RPG_Ammo_To_Give );
+				player.SetAmmo( AmmoType.Grenade, Grenades_To_Give );
+				player.SetAmmo( AmmoType.SLAM, Slams_To_Give );
+
 
 				player.ActiveChild = player.Inventory.GetSlot( Rand.Int( 0, player.Inventory.Count() - 1 ) );
 				player.DidRespawn = false;
